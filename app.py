aeb93ce38aac95906943512ee139f8e4e16afbef
JANIO ALMEIDA <JANIO9469@IOT>
2024-10-28 17:08:29 -0300

    SGE

---
+++ app.py
@@ -0,0 +1,36 @@
+from pkgutil import iter_importers
+from flask import Flask
+from models import db
+from config import Config
+from controllers.usuario_controller import usuario_bp
+from controllers.produto_controller import produto_bp
+from controllers.cliente_controller import cliente_bp
+from controllers.pedido_controller import pedido_bp
+from controllers.detalhepedido_controller import detalhePedido_bp
+
+
+
+
+def criar_app():
+   
+    app = Flask(__name__)
+
+    app.config.from_object(Config)
+
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+
+    app.register_blueprint(usuario_bp)
+    app.register_blueprint(produto_bp)
+    app.register_blueprint(cliente_bp)
+    app.register_blueprint(pedido_bp)
+    app.register_blueprint(detalhePedido_bp)
+   
+
+    app.run(debug=True)
+    
+
+if __name__ == '__main__':
+    app = criar_app()
